0d4fcc6081f5dbf0bb66c57820e209beae89f5d3
gcscreations <gcscreations.ar.game@gmail.com>
2023-09-19 13:24:02 +0530

    First release of Hello World!

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './App.css';
+import MindARViewer from './mindar-viewer';
+import MindARThreeViewer from './mindar-three-viewer';
+
+function App() {
+  const [started, setStarted] = useState("aframe");
+
+  return (
+    <div className="App">
+        {started === 'aframe' && (
+            <div style={{ width: "100vw", height: "100vh" }} className="container">
+                <MindARViewer/>
+                <video></video>
+            </div>
+        )}
+      {/*<h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
+
+      <div className="control-buttons">
+        {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
+        {started === null && <button onClick={() => {setStarted('three')}}>Start ThreeJS version</button>}
+        {started !== null && <button onClick={() => {setStarted(null)}}>Stop</button>}
+      </div>
+
+
+
+      {started === 'three' && (
+        <div className="container">
+          <MindARThreeViewer />
+        </div>
+      )}*/}
+    </div>
+  );
+}
+
+export default App;
