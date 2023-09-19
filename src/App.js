884e569f59019bfd87df9e05ebbe585b59b2f9ef
gcscreations <gcscreations.ar.game@gmail.com>
2023-09-19 14:00:03 +0530

    UPDATED GIT!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import MindARViewer from './mindar-viewer';
-import MindARThreeViewer from './mindar-three-viewer';
+//import MindARThreeViewer from './mindar-three-viewer';
 
 function App() {
   const [started, setStarted] = useState("aframe");
